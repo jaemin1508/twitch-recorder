21db37d7577506dcebd5c1d5b208d909afd28ec0
jaemin1508 <jaemin1508@naver.com>
2023-03-20 21:16:58 +0900

    fix ✨

--- index.ts
+++ index.ts
@@ -2,8 +2,12 @@ import WebSocket from "ws";
 import axios from "axios";
 import chalk from "chalk";
 import fs from "fs";
+import readline from "readline/promises";
 
-const streamer = "papan030";
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 async function getPlaybackAccessToken(streamer_id: string): Promise<{ signature: string; token: string; }> {
     const { data: { data: { streamPlaybackAccessToken: { value: token, signature } } } } = await axios({
@@ -52,111 +56,128 @@ function numberURLs(m3u8: string) {
     return urls.map((url: string) => { return { url, sequence: sequence++ } });
 }
 
-function decodeM3U8(m3u8: string): string[] {
-    return m3u8.split("\n").filter((line: string) => line.startsWith("https://"));
-}
+(async () => {
+    const streamer = await rl.question("Enter streamer name: ");
+    
+    if (!fs.existsSync("./sequences")) fs.mkdirSync("./sequences");
+    
+    let messageId = "";
+    let channelId = "";
+    let sessionId = "";
+    let signature = "", token = "";
+    let online = false;
+    
+    console.log('waiting for socket connection ...');
 
-if (!fs.existsSync("./sequences")) fs.mkdirSync("./sequences");
-
-let channelId = "";
-let sessionId = "";
-let signature = "", token = "";
-let online = false;
-
-const ws = new WebSocket("wss://pubsub-edge.twitch.tv");
-
-ws.on("open", () => {
-    console.log('socket opened, initiating listener ...');
-
-    setInterval(() => {
-        ws.send(JSON.stringify({
-            type: "PING"
-        }));    
-    }, 1000 * 60 *3);
-
-    getPlaybackAccessToken(streamer).then(async ({ signature: _signature, token: _token }) => {
-        signature = _signature;
-        token = _token;
-        channelId = JSON.parse(token).channel_id;
-        console.log(`signature: ${signature}`);
-        console.log(`token: ${token}`);
-        console.log(`channel id: ${channelId}`);
-        
-        ws.send(JSON.stringify({
-            type: "LISTEN",
-            nonce: "IkgLiKVbRnraWWdbA4I2nodIiAsrHo",
-            data: {
-                topics: [
-                    `video-playback-by-id.${channelId}`
-                ]
-            }
-        }));
-        
-        console.log('checking if stream is online...');
-        await get(getM3U8Url(streamer, token, signature)).then(async (lines) => {
-            const videodata = await get(filterURL(lines)[0]);
-            sessionId = videodata.find((line: string) => line.match(/X-TV-TWITCH-SESSIONID="(\d+)"/))!.match(/X-TV-TWITCH-SESSIONID="(\d+)"/)![1]!;
-            console.log(`session Id: ${sessionId}`);
-            fs.mkdirSync(`./sequences/${sessionId}`, { recursive: true });
-            m3u8 = filterURL(lines)[0];
-            online = true;
-            console.log("stream is online");
-        }).catch(() => {
-            online = false;
-            console.log("stream is offline");
-        });
-    })
-    .catch((e) => {
-        console.log(e);
-        console.log("error getting playback access token");
-        return;
-    });
-});
-
-let m3u8 = "";
-
-setInterval(async () => {
-    if (online) {
-        const data = await get(m3u8).catch(() => null);
-        if (!data) {
-            console.log("stream is offline");
-            return;
-        }
-        const urls = numberURLs(data.join("\n"));
-        for (let url of urls) {
-            if (fs.existsSync(`./sequences/${sessionId}/${url.sequence}.ts`)) continue;
-            process.stdout.write(`[${chalk.green(url.sequence)}] downloading ${chalk.gray(url.url).substring(0, process.stdout.columns - 18)}${chalk.white('...')}`);
-            const { data } = await axios(url.url, { responseType: "arraybuffer" });
-            process.stdout.clearLine(0);
-            process.stdout.cursorTo(0);
-            process.stdout.write(`[${chalk.green(url.sequence)}] downloaded\n`);
-            fs.createWriteStream(`./sequences/${sessionId}/${url.sequence}.ts`).write(data);
-        }
-    }
-}, 2000);
-
-ws.on("message", async buffer => {
-    console.log(buffer.toString());
-    const { type, data } = JSON.parse(buffer.toString());
-    if (type === "MESSAGE") {
-        if (data) {
-            const { type: data_type } = JSON.parse(data.message);
-            if (data_type === "stream-down") {
-                console.log("stream went offline");
-                online = false;
-            } else if (data_type === "stream-up") {
-                console.log("stream went online");
-                const { signature: _signature, token: _token } = await getPlaybackAccessToken(streamer);
-                signature = _signature;
-                token = _token;
-                console.log(`signature: ${signature}, token: ${token}`);
-                m3u8 = filterURL(await get(getM3U8Url(streamer, token, signature)))[0];
-                console.log('m3u8: ' + m3u8);
-                sessionId = (await get(m3u8)).find((line: string) => line.match(/X-TV-TWITCH-SESSIONID="(\d+)"/g))!.match(/X-TV-TWITCH-SESSIONID="(\d+)"/g)![1]!;
-                fs.mkdirSync(`./sequences/${sessionId}`, { recursive: true });
+    let ws = new WebSocket("wss://pubsub-edge.twitch.tv");
+    
+    function openHandler() {
+        console.log('socket opened, initiating listener ...');
+    
+        setInterval(() => {
+            ws.send(JSON.stringify({
+                type: "PING"
+            }));    
+        }, 1000 * 60 *3);
+    
+        getPlaybackAccessToken(streamer).then(async ({ signature: _signature, token: _token }) => {
+            signature = _signature;
+            token = _token;
+            channelId = JSON.parse(token).channel_id;
+            console.log(`signature: ${signature}`);
+            console.log(`token: ${token}`);
+            console.log(`channel id: ${channelId}`);
+            
+            ws.send(JSON.stringify({
+                type: "LISTEN",
+                nonce: "IkgLiKVbRnraWWdbA4I2nodIiAsrHo",
+                data: {
+                    topics: [
+                        `video-playback-by-id.${channelId}`
+                    ]
+                }
+            }));
+            
+            console.log('checking if stream is online...');
+            await get(getM3U8Url(streamer, token, signature)).then(async (lines) => {
+                sessionId = `${streamer}-${Date.now()}`;
                 console.log(`session Id: ${sessionId}`);
+                fs.mkdirSync(`./sequences/${sessionId}`, { recursive: true });
+                m3u8 = filterURL(lines)[0];
                 online = true;
+                console.log("stream is online");
+            }).catch(() => {
+                online = false;
+                console.log("stream is offline");
+            });
+        })
+        .catch((e) => {
+            console.log(e);
+            console.log("error getting playback access token");
+            return;
+        });
+    };
+    
+    let m3u8 = "";
+    let lastSequence = 0;
+
+    setInterval(async () => {
+        if (online) {
+            const data = await get(m3u8).catch(() => null);
+            if (!data) {
+                console.log("stream is offline");
+                return;
+            }
+            const urls = numberURLs(data.join("\n"));
+            for (let url of urls) {
+                if (fs.existsSync(`./sequences/${sessionId}/${url.sequence}.ts`)) continue;
+                process.stdout.write(`[${chalk.green(url.sequence)}] downloading ${chalk.gray(url.url).substring(0, process.stdout.columns - 18)}${chalk.white('...')}`);
+                const { data } = await axios(url.url, { responseType: "arraybuffer" });
+                process.stdout.clearLine(0);
+                process.stdout.cursorTo(0);
+                process.stdout.write(`[${chalk.green(url.sequence)}] downloaded\n`);
+                fs.createWriteStream(`./sequences/${sessionId}/${url.sequence}.ts`).write(data);
+                lastSequence = url.sequence;
             }
         }
-    }
-});
+    }, 2000);
+
+    async function messageHandler(buffer: Buffer) {
+        console.log(buffer.toString());
+        const { type, data, nonce } = JSON.parse(buffer.toString());
+        if (type === "MESSAGE") {
+            if (data) {
+                const { type: data_type } = JSON.parse(data.message);
+                if (data_type === "stream-down") {
+                    console.log("stream went offline");
+                    online = false;
+                } else if (data_type === "stream-up") {
+                    console.log("stream went online");
+                    const { signature: _signature, token: _token } = await getPlaybackAccessToken(streamer);
+                    signature = _signature;
+                    token = _token;
+                    console.log(`signature: ${signature}, token: ${token}`);
+                    m3u8 = filterURL(await get(getM3U8Url(streamer, token, signature)))[0];
+                    console.log('m3u8: ' + m3u8);
+                    sessionId = `${streamer}-${Date.now()}`;
+                    fs.mkdirSync(`./sequences/${sessionId}`, { recursive: true });
+                    console.log(`session Id: ${sessionId}`);
+                    online = true;
+                }
+            }
+        } else if (type === "RESPONSE") {
+            if (nonce === "IkgLiKVbRnraWWdbA4I2nodIiAsrHo") {
+                console.log("listener initiated");
+            }
+        } else if (type === "RECONNECT") {
+            console.log("reconnecting ...");
+            ws.close();
+            ws = new WebSocket("wss://pubsub-edge.twitch.tv");
+            ws.on("open", openHandler);
+            ws.on("message", messageHandler);
+        }
+    };
+    
+    ws.on("open", openHandler);
+    ws.on("message", messageHandler);
+})();
